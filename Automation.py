519ee82cfa68e606b39d5d72c9959407c0825a0d
arnavlikhar <74099853+arnavlikhar@users.noreply.github.com>
2021-05-18 13:44:52 +0530

    Add files via upload

---
+++ Automation.py
@@ -0,0 +1,7 @@
+  
+import os
+search = input('Enter aplication name:- ')
+if(search == "Task"or "task" or "TASK"):
+    print("Opening Task Aplication")
+    os.system(r"C:/Users/vijayrlikhar/Desktop/python introduction programming/Task.txt")
+    
